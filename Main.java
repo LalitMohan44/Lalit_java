5d18cae849565bbb7692eed62d1a1d0a6748dc73
Lalit Mohan <lalit.mohan@enquero.com>
2019-06-17 22:11:20 +0530

    Modified

--- Main.java
+++ Main.java
@@ -2,6 +2,8 @@ package Lalit;
 
 public class Main {
     public static void main(String[] args){
-        System.out.println("Hello Word");
+
+        String myString="This is Lalit.";
+        System.out.println("Hello World "+myString);
     }
 }

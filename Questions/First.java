c31aa29c8dfed80a28f1351e65c6d476e352bf77
Lalit Mohan <lalit.mohan@enquero.com>
2019-06-18 20:17:50 +0530

    First Question

--- Questions/First.java
+++ Questions/First.java
@@ -1,4 +1,20 @@
 package Lalit.Lalit_java.Questions;
 
-public class First {
+ class Super {
+    public void display() throws RuntimeException { // Super class declare an exception
+
+        System.out.println("This is Parent Class");
+    }
+}
+
+ public class First extends Super {
+    public void display() throws ArithmeticException { //Sub Class(First) needs to declare a child exception or same
+                                                       // but not a broader exception of RuntimeException
+        System.out.println("This is Child Class");
+    }
+
+    public  static void main(String args[]){
+        Super c= new First();
+        c.display();
+    }
 }

72f49498161a2064d0e5976fc7f6a8b755b8f0d6
Lalit Mohan <lalit.mohan@enquero.com>
2019-06-19 00:40:46 +0530

    Employee Class

---
+++ Questions/Employee.java
@@ -0,0 +1,68 @@
+package Lalit.Lalit_java.Questions;
+
+public class Employee {
+
+    String name;
+    int id;
+    int age;
+    String designation;
+    String technology;
+    double salary;
+
+    public Employee(String name) {
+        this.name = name;
+    }
+
+    public void empId(int empId) {
+        id = empId;
+    }
+
+    public void empAge(int empAge) {
+        age = empAge;
+    }
+
+    public void empDesignation(String empDesig) {
+        designation = empDesig;
+    }
+
+    public void empTecnology(String empTech) {
+        technology = empTech;
+    }
+
+    public void empSalary(double empSalary) {
+        salary = empSalary;
+    }
+
+    public void printEmployee() {
+        System.out.println("Name:"+ name );
+        System.out.println("Employee Id:"+ id);
+        System.out.println("Age:" + age );
+        System.out.println("Designation:" + designation );
+        System.out.println("Technology:"+ technology);
+        System.out.println("Salary:" + salary);
+    }
+}
+
+class EmployeeAdd {
+
+    public static void main(String args[]) {
+        /* Create two objects using constructor */
+        Employee empOne = new Employee("Lalit Mohan");
+        Employee empTwo = new Employee("Keshav Sharma");
+
+        empOne.empId(12345);
+        empOne.empAge(21);
+        empOne.empDesignation("Senior Software Engineer");
+        empOne.empTecnology("Java");
+        empOne.empSalary(100000);
+        empOne.printEmployee();
+
+        empTwo.empId(23456);
+        empTwo.empAge(22);
+        empTwo.empDesignation("Software Engineer");
+        empTwo.empTecnology("SQL");
+        empTwo.empSalary(50000);
+        empTwo.printEmployee();
+    }
+}
+

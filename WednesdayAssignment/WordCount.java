99d476e649a64ce16ef2682c3b795fe2eb2a91a1
Lalit Mohan <lalit.mohan@enquero.com>
2019-06-19 17:36:28 +0530

    Count each word occurence in String

---
+++ WednesdayAssignment/WordCount.java
@@ -0,0 +1,36 @@
+package Lalit.Lalit_java.WednesdayAssignment;
+
+import java.util.HashMap;
+
+public class WordCount {
+
+    public static void main(String args[]) {
+        String s1 = "java string split method by java string split";
+        wordCount(s1);
+
+    }
+
+    static void wordCount(String inputString)
+    {
+        HashMap<String, Integer> wordCountMap = new HashMap<>();
+
+        String[] words = inputString.split("\\s"); //splits the string based on whitespace
+
+        for (String w : words) {
+
+            if (wordCountMap.containsKey(w)) {
+
+                wordCountMap.put(w, wordCountMap.get(w) + 1);
+            }
+            else {
+
+                wordCountMap.put(w, 1);
+            }
+        }
+
+        // Printing the wordCountMap
+        System.out.println(wordCountMap);
+
+    }
+
+}

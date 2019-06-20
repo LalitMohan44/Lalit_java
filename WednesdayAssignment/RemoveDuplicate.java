299e42a8ed4d238ccca8b935f8ff7e8bd94982d3
Lalit Mohan <lalit.mohan@enquero.com>
2019-06-20 15:25:52 +0530

    2nd letter, 2nd occurrence

--- WednesdayAssignment/RemoveDuplicate.java
+++ WednesdayAssignment/RemoveDuplicate.java
@@ -9,12 +9,13 @@ public class RemoveDuplicate {
 
         Book one = new Book(12345,"JAVA","Computer","Lalit");
         Book two = new Book(12365, "Python", "Computer", "Mohan");
+        Book three = new Book(12345,"JAVA","Computer","Lalit");
 
         map.put(1, one);
         map.put(2, two);
         map.put(3, one);
 
-        System.out.println("HashMap with duplicate Books: "+map);
+        System.out.println("Removing Duplicate in HashMap  ");
 
         //Removing duplicate in HashMap
         Collection<Book> list = map.values();
@@ -25,22 +26,28 @@ public class RemoveDuplicate {
                 itr.remove();
             }
         }
-        System.out.println("HashMap without duplicate Books: "+map);
+        for (Map.Entry<Integer, Book> entry:map.entrySet()){
+            Book b = entry.getValue();
+            System.out.println(b.author+" "+b.title);
+        }
 
         //Removing by HashSet
-        HashSet set = new HashSet();
+        System.out.println("Removing Duplicate in Hashset using hashCode and equals ");
+        HashSet<Book> set = new HashSet();
         set.add(one);
         set.add(two);
-        set.add(one);       //HashSet will not take duplicate objects
+        set.add(three);
 
-        System.out.println(set);
 
+        for(Book b:set ) {
+            System.out.println(b.author+" "+b.title);
+        }
 
 
     }
 }
 
-class Book{
+class Book {
     String title;
     String category;
     int bookId;
@@ -53,6 +60,22 @@ class Book{
         this.title = title;
         this.category= category;
         this.author= author;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return bookId == book.bookId &&
+                Objects.equals(title, book.title) &&
+                Objects.equals(category, book.category) &&
+                Objects.equals(author, book.author);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, category, bookId, author);
     }
 
 }
